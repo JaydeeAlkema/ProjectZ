13d94b9ec6cd53c9cb3b903622b4394cb63b22b3
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-04-11 17:31:41 +0200

    Add CreateOrJoinRoomCanvas Behaviour
    Handles all the behaviour of the Create or Join Room canvas menu

---
+++ Assets/Scripts/UI/Rooms/CreateOrJoinRoomCanvas.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateOrJoinRoomCanvas : MonoBehaviour
+{
+	[SerializeField] private CreateRoomMenu _createRoomMenu;
+	private RoomsCanvases _roomsCanvases;
+
+	public void FirstInitialize(RoomsCanvases canvases)
+	{
+		_roomsCanvases = canvases;
+		_createRoomMenu.FirstInitialize(canvases);
+	}
+}
