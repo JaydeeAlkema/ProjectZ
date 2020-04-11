fe7b03017151eba3ce258bc47e13269e8af0655c
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-04-11 13:52:10 +0200

    Add GameSettings.cs

---
+++ Assets/Scripts/Managers/GameSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Manager/GameSettings")]
+public class GameSettings : ScriptableObject
+{
+	#region Variables
+	[SerializeField] private string _gameVersion = "0.0.0";
+	[SerializeField] private string _nickname = "DevJ";
+	#endregion
+
+	#region Properties
+	public string GameVersion { get => _gameVersion; }
+	public string Nickname { get => _nickname + Random.Range(0, int.MaxValue).ToString(); }
+	#endregion
+}
