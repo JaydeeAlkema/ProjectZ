802ab8b3b5ddd0661f503cd9355d3f6bc0ac73d1
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-04-11 13:12:06 +0200

    Add Base Files

---
+++ Assets/Scripts/Networking/ConnectionTest.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionTest : MonoBehaviourPunCallbacks
+{
+	private void Start()
+	{
+		Debug.Log("Connecting to server...");
+		PhotonNetwork.GameVersion = "0.0.1";
+		PhotonNetwork.ConnectUsingSettings();
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log("Connected to server!");
+	}
+
+	public override void OnDisconnected(DisconnectCause cause)
+	{
+		Debug.Log("Disconnected from server for reason " + cause.ToString());
+	}
+}
