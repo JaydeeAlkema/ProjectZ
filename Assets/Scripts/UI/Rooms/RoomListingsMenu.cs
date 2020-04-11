543ff50a7df75efd7d78106a701ba9efdb33d6ea
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-04-11 17:32:42 +0200

    Add Room Listing behaviour

---
+++ Assets/Scripts/UI/Rooms/RoomListingsMenu.cs
@@ -0,0 +1,40 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomListingsMenu : MonoBehaviourPunCallbacks
+{
+	[SerializeField] private Transform _content;
+	[SerializeField] private RoomListing _roomListing;
+
+	private List<RoomListing> _listings = new List<RoomListing>();
+
+	public override void OnRoomListUpdate(List<RoomInfo> roomList)
+	{
+		foreach(RoomInfo info in roomList)
+		{
+			// Removed from rooms list.
+			if(info.RemovedFromList)
+			{
+				int index = _listings.FindIndex(x => x.RoomInfo.Name == info.Name);
+				if(index != -1)
+				{
+					Destroy(_listings[index].gameObject);
+					_listings.RemoveAt(index);
+				}
+			}
+			// Added from rooms list.
+			else
+			{
+				RoomListing listing = Instantiate(_roomListing, _content);
+				if(listing != null)
+				{
+					listing.SetRoomInfo(info);
+					_listings.Add(listing);
+				}
+			}
+		}
+	}
+}
