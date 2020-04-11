2f2cba4d085f2a6060cbd2c21492060a6d7a4f79
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-04-11 17:32:30 +0200

    Add Roomlisting behaviour

---
+++ Assets/Scripts/UI/Rooms/RoomListing.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RoomListing : MonoBehaviour
+{
+	[SerializeField] private TextMeshProUGUI _text;
+
+	public RoomInfo RoomInfo { get; private set; }
+
+	public void SetRoomInfo(RoomInfo roomInfo)
+	{
+		RoomInfo = roomInfo;
+		_text.text = roomInfo.MaxPlayers + ", " + roomInfo.Name;
+	}
+}
