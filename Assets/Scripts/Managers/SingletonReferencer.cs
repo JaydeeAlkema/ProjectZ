9b6893c7947427ae12a42f42a87a8f4e5b72f328
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-04-11 13:53:47 +0200

    Add SingletonReferencer
    This makes sure that the singleton is created when the scene is launched.

---
+++ Assets/Scripts/Managers/SingletonReferencer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonReferencer : MonoBehaviour
+{
+	[SerializeField] private MasterManager _masterManager;
+}
