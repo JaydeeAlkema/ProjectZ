3e30d3b9e71b2a0c76dd5c6f4f738adfbf8c50f5
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-04-11 17:32:55 +0200

    Add Rooms canvases behaviour

---
+++ Assets/Scripts/UI/Rooms/RoomsCanvases.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomsCanvases : MonoBehaviour
+{
+	#region Variables
+	[SerializeField] private CreateOrJoinRoomCanvas _createOrJoinRoomCanvas;
+	[SerializeField] private CurrentRoomCanvas _currentRoomCanvas;
+	#endregion
+
+	#region Properties
+	public CreateOrJoinRoomCanvas CreateOrJoinRoomCanvas { get { return _createOrJoinRoomCanvas; } }
+	public CurrentRoomCanvas CurrentRoomCanvas { get { return _currentRoomCanvas; } }
+	#endregion
+
+	private void Awake()
+	{
+		FirstInitialize();
+	}
+
+	void FirstInitialize()
+	{
+		CreateOrJoinRoomCanvas.FirstInitialize(this);
+		CurrentRoomCanvas.FirstInitialize(this);
+	}
+}
